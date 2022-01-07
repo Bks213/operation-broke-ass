f598c5a90d54ce4ee0f26554ed69650c61e96a05
Bks213 <47408814+Bks213@users.noreply.github.com>
2022-01-07 01:18:58 -0500

    Update achatfutures.py

--- FICHIER DE TEST/achatfutures.py
+++ FICHIER DE TEST/achatfutures.py
@@ -6,12 +6,20 @@ import requests
 from requests.models import Response
 import pprint
 from ftx import FtxClient
+import ccxt
+
+
+client = ftx.FtxClient(api_key="TEiyW-MuA9al-cLgAw32nhEGrt9LgWY1K47-tHCh", api_secret="6PTcHT7s_gesH36U0nLbg4XgWhEMunxWQwfIM9ES")
 
 
 
 
-
-
+def getcryptosize(cryptoname):
+    cryptoname = requests.get('https://ftx.com/api/markets/'+cryptoname+'-PERP').json()
+    price=(cryptoname["result"]['price'])
+    quantitepre=(10/price)
+    quantite=round(quantitepre,5)
+    print(quantitepre)
 
 
 def short():
@@ -29,7 +37,22 @@ def long():
     api_secret = '6PTcHT7s_gesH36U0nLbg4XgWhEMunxWQwfIM9ES'
     client = FtxClient(api_key=api_key, api_secret=api_secret)
     symbol = 'BTC-PERP'
+    type='stop_limit'
     side = 'buy'
     qty = 0.00021
     price = 12345.0
-    client.place_order(symbol, side, price, qty)
+    client.place_order(type,symbol, side, price, qty)
+    
+
+ccxt_bot = ccxt.ftx({
+    'enableRateLimit': True,
+    'apiKey': 'TEiyW-MuA9al-cLgAw32nhEGrt9LgWY1K47-tHCh',
+    'secret': '6PTcHT7s_gesH36U0nLbg4XgWhEMunxWQwfIM9ES', 
+})
+
+
+coin='BTC-PERP'
+amount=0.00019
+stopPrice="40000"
+
+ccxt_bot.create_order(coin, 'stop', stopPrice, 'buy', amount, {'leverage': 5})
