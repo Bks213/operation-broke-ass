20e5f0cd5edf19a171dac630a0f4c5ff0e9ea6d0
Bks213 <47408814+Bks213@users.noreply.github.com>
2022-01-10 13:10:07 -0500

    Update achatfutures.py

--- FICHIER DE TEST/achatfutures.py
+++ FICHIER DE TEST/achatfutures.py
@@ -1,48 +1,12 @@
 from sys import int_info
-import ftx
 import pprint
 import json
 import requests
 from requests.models import Response
 import pprint
-from ftx import FtxClient
 import ccxt
 from telegram import *
-
-
-client = ftx.FtxClient(api_key="TEiyW-MuA9al-cLgAw32nhEGrt9LgWY1K47-tHCh", api_secret="6PTcHT7s_gesH36U0nLbg4XgWhEMunxWQwfIM9ES")
-
-
-
-
-def getcryptosize(cryptoname):
-    cryptoname = requests.get('https://ftx.com/api/markets/'+cryptoname+'-PERP').json()
-    price=(cryptoname["result"]['price'])
-    quantitepre=(10/price)
-    quantite=round(quantitepre,5)
-    print(quantitepre)
-
-
-def short():
-    api_key = 'TEiyW-MuA9al-cLgAw32nhEGrt9LgWY1K47-tHCh'
-    api_secret = '6PTcHT7s_gesH36U0nLbg4XgWhEMunxWQwfIM9ES'
-    client = FtxClient(api_key=api_key, api_secret=api_secret)
-    symbol = 'BTC-PERP'
-    side = 'sell'
-    qty = 0.00021
-    price = 12345.0
-    client.place_order(symbol, side, price, qty)
-
-def long():
-    api_key = 'TEiyW-MuA9al-cLgAw32nhEGrt9LgWY1K47-tHCh'
-    api_secret = '6PTcHT7s_gesH36U0nLbg4XgWhEMunxWQwfIM9ES'
-    client = FtxClient(api_key=api_key, api_secret=api_secret)
-    symbol = 'BTC-PERP'
-    side = 'stop'
-    qty = 0.00021
-    price = 12345.0
-    client.place_order(type,symbol, side, price, qty)
-
+from info import *
 
 ccxt_bot = ccxt.ftx({
     'enableRateLimit': True,
@@ -51,11 +15,41 @@ ccxt_bot = ccxt.ftx({
 })
 
 
-coin='BTC-PERP'
-amount=0.00019
-stopPrice="40000"
+def balancecompte():
+    infocompte=(ccxt_bot.fetch_balance())
+    global balance
+    balance=(float(infocompte['info']['result'][2]['free']))
+
+
+
+def getcryptosize(cryptoname):
+    balancecompte()
+    requete = requests.get('https://ftx.com/api/markets/'+cryptoname+'-PERP').json()
+    price=(requete['result']['price'])
+    global montantautilisé
+    montantautilisé=(pourcentage*balance)/100
+    global quantite
+    quantite=(montantautilisé/price)
+    print(quantite)
+
+
+
+
+cryptoaacheter='SOL'
+
+
+
+def long():
+    getcryptosize(cryptoaacheter)
+    coin=cryptoaacheter+"-PERP"
+    amount=quantite
+    ccxt_bot.create_order(coin,'market','buy',amount,params={'leverage': 15})
+    requete = requests.get('https://ftx.com/api/markets/'+cryptoaacheter+'-PERP').json()
+    prix=(requete['result']['price'])
+    stoploss=(prix-(prix*stoplosspourcentage/100))
+    #ccxt_bot.create_order(coin,'stop','sell',amount,params={'triggerPrice':139})
+    send_message_on_telegram_long(coin,str(montantautilisé))
+
 
 long()
 
-#ccxt_bot.create_order(coin, 'stop', 'buy', amount, {'leverage': 5})
-#ccxt_bot.create_order('SOL-PERP','stop','sell',0.05583472920156338,params={'triggerPrice':135.00,'leverage': 5,'reduceOnly': True})
